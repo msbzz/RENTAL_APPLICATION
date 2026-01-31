66b42954addec3ec0e5ec55624d315c4efdb4d37
Marco Barozzi <mbzstudent@gmail.com>
2026-01-31 10:58:49 -0300

    tela de registro (register screen) part 2

--- lib/views/auth/register_screen.dart
+++ lib/views/auth/register_screen.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:rental_application/controllers/auth_controller.dart';
+import 'package:rental_application/core/constants/color_constants.dart';
+import 'package:rental_application/models/user_model.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
   @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Register Screen')));
+    final _emailController = TextEditingController();
+    final _passwordController = TextEditingController();
+    final _confirmPasswordController = TextEditingController();
+    final _authController = AuthController();
+    final _formKey = GlobalKey<FormState>();
+    bool _obscurePassword = false;
+    bool _obscureConfirmePassword = false;
+    bool _isLoading = false;
+    UserRole _selectedRole = UserRole.tenant;
+
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.w),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 20.h),
+                  IconButton(
+                    onPressed: () => context.pop(),
+                    icon: Container(
+                      padding: EdgeInsets.all(8.w),
+                      decoration: BoxDecoration(
+                        color: AppColors.surface,
+                        borderRadius: BorderRadius.circular(12.r),
+                      ),
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        size: 20.sp,
+                        color: AppColors.textPrimary,
+                      ),
+                    ),
+                    iconSize: 36.sp,
+                  ),
+                  SizedBox(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
