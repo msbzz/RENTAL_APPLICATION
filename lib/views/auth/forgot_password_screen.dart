b876d7ac3fdf0f15fc1fb9796ee035ecbaf1bfb2
Marco Barozzi <mbzstudent@gmail.com>
2026-02-01 06:26:42 -0300

    tela recuperação de senha (forgot password screen) part2

--- lib/views/auth/forgot_password_screen.dart
+++ lib/views/auth/forgot_password_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:rental_application/controllers/auth_controller.dart';
+import 'package:rental_application/core/constants/color_constants.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({super.key});
@@ -8,8 +12,49 @@ class ForgotPasswordScreen extends StatefulWidget {
 }
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _authController = AuthController();
+  bool _isLoading = false;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Forgot Password')));
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 24.w),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20.h),
+              IconButton(
+                onPressed: () => context.pop(),
+                icon: Container(
+                  padding: EdgeInsets.all(8.w),
+                  decoration: BoxDecoration(
+                    color: AppColors.surface,
+                    borderRadius: BorderRadius.circular(12.r),
+                  ),
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    size: 20.sp,
+                    color: AppColors.textPrimary,
+                  ),
+                ),
+                iconSize: 36.sp,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
