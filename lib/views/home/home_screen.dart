dfd2d9388242c55338519c3856986312351996ed
Marco Barozzi <mbzstudent@gmail.com>
2026-02-02 21:34:29 -0300

    tela de locação (property card) part17

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -98,6 +98,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
             ),
+            SliverPadding(padding: EdgeInsets.only(bottom: 16.h)),
           ],
         ),
       ),
